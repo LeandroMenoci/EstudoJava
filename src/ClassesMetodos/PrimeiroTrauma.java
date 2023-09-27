66c6c4f301cf82ec7de555a03a1ee87e28dce35e
leandromenoci <menoci.leandro@gmail.com>
2023-09-27 09:55:17 -0300

    This e This()

---
+++ src/ClassesMetodos/PrimeiroTrauma.java
@@ -0,0 +1,12 @@
+package ClassesMetodos;
+
+public class PrimeiroTrauma {
+
+  int a = 3; // Desafio, não pode mexer aqui.
+
+  public static void main(String[] args) {
+
+    var a1 = new PrimeiroTrauma(); // Necessário primeiro instanciar
+    System.out.println(a1.a); // então é possível chamar a variavel 'a'
+  }
+}
